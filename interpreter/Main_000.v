535e2a8fd3485be734e0b6876a8341c47688b576
Hexirp <Hexirp@users.noreply.github.com>
2021-05-27 20:55:52 +0900

    Add: Finite_List を

--- interpreter/Main_000.v
+++ interpreter/Main_000.v
@@ -60,3 +60,38 @@ Module Plus_000.
 End Plus_000.
 
 Definition plus (m n : Nat) : Nat := Plus_000.function_2 m n.
+
+Set Universe Polymorphism.
+
+Module Finite_List_000.
+
+  Inductive Tuple_0@{i | } : Type@{i} := tuple_0 : Tuple_0.
+
+  Inductive Tuple_1@{i | } (A : Type@{i}) : Type@{i} := tuple_1 : A -> Tuple_1 A.
+
+  Inductive Tuple_2@{i | } (A B : Type@{i}) : Type@{i} := tuple_2 : A -> B -> Tuple_2 A B.
+
+  Inductive Tuple_3@{i | } (A B C : Type@{i}) : Type@{i} := tuple_3 : A -> B -> C -> Tuple_3 A B C.
+
+  Fixpoint Type_0@{i | } (x : Bool_String) (A : Type@{i}) {struct x} : Type@{i}
+    :=
+      match x with
+        | nil => Tuple_1@{i} A
+        | cons xp xs
+          =>
+            match xp with
+              | false => Tuple_2@{i} (Type_0 xs A) (Type_0 xs A)
+              | true => Tuple_3@{i} A (Type_0 xs A) (Type_0 xs A)
+            end
+      end.
+
+  Definition Type_1@{i | } (n : Nat) (A : Type@{i}) : Type@{i}
+    :=
+      match n with
+        | zero => Tuple_0@{i}
+        | ones n_ => Type_0@{i} n_ A
+      end.
+
+End Finite_List_000.
+
+Definition Finite_List@{i | } (n : Nat) (A : Type@{i}) : Type@{i} := Finite_List_000.Type_1@{i} n A.
