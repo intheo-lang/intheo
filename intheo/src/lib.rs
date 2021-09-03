54a10dbfbf03675711d384ecec05783c9a06455a
Hexirp <Hexirp@users.noreply.github.com>
2021-09-04 00:58:05 +0900

    Update: ドキュメントを更新する

--- intheo/src/lib.rs
+++ intheo/src/lib.rs
@@ -1,8 +1,8 @@
-pub struct Port { id : u32 }
+/// `Address` である。
+pub struct Address { pub id : u32 }
 
-pub struct Address { id : u32 }
-
-pub struct Slot { id : u32 }
+/// `Slot` である。値は `ERA` と `CON` と `FAN` の三種類である。
+pub struct Slot { pub id : u32 }
 
 /// `00` である。
 pub const ERA : Slot = Slot { id : 0 };
@@ -12,6 +12,9 @@ pub const CON : Slot = Slot { id : 1 };
 
 /// `10` である。
 pub const FAN : Slot = Slot { id : 2 };
+
+/// `Port` である。 `Address` と `Slot` のペアである。
+pub struct Port { pub id : u32 }
 
 /// `Port` を構築する。
 pub fn port(node : Address, slot : Slot) -> Port
