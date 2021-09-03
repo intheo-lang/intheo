f031fa787caf4b54e962a00052bbe3d748223cde
Hexirp <Hexirp@users.noreply.github.com>
2021-09-04 00:52:05 +0900

    Add: Port と Address と Slot を定義する
    [1] より。
    
    [1]: https://github.com/MaiaVictor/Symmetric-Interaction-Calculus/blob/1e74be3666bf616fe2fc2b3b401db72d67a22c2d/src/net.rs

--- intheo/src/lib.rs
+++ intheo/src/lib.rs
@@ -1,3 +1,32 @@
-fn foo() {
-    println!("Hello, world!");
-}
+pub struct Port { id : u32 }
+
+pub struct Address { id : u32 }
+
+pub struct Slot { id : u32 }
+
+/// `00` である。
+pub const ERA : Slot = Slot { id : 0 };
+
+/// `01` である。
+pub const CON : Slot = Slot { id : 1 };
+
+/// `10` である。
+pub const FAN : Slot = Slot { id : 2 };
+
+/// `Port` を構築する。
+pub fn port(node : Address, slot : Slot) -> Port
+  {
+    Port { id : (node.id << 2) | slot.id }
+  }
+
+/// `Port` から `Address` を取り出す。
+pub fn addr(port : Port) -> Address
+  {
+    Address { id : port.id >> 2 }
+  }
+
+/// `Port` から `Slot` を取り出す。
+pub fn slot(port : Port) -> Slot
+  {
+    Slot { id : port.id & 3 }
+  }
