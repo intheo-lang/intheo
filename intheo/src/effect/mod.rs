5c85332dd4b591502c490ad50a252d080362e027
Hexirp <Hexirp@users.noreply.github.com>
2021-09-29 01:45:04 +0900

    Update: effect モジュールにドキュメントをつける

--- intheo/src/effect/mod.rs
+++ intheo/src/effect/mod.rs
@@ -1,3 +1,6 @@
+/// 作用を伴って生成された値である。
+///
+///  疑似的に Haskell の IO 型を再現する。ただし、実際には Haskell の IO 型と違って作用の閉じ込めは行われない。そのため、 `Effect` 型の値は `run` メソッドで即時に取り出さなければならない。
 #[derive(Clone, Debug)]
 pub struct Effect<T>
   {
@@ -5,6 +8,7 @@ pub struct Effect<T>
   }
 
 impl<T> Effect<T> {
+  /// 値を取り出す。
   fn run(self) -> T
     {
       let Effect { value : self_value } = self
